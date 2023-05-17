7106fb5c2a7339b0aee7df82c38cbb841c5b1f65
Jonathan Poli <epideixx@Jonathans-MacBook-Air.local>
2023-05-17 12:29:40 -0400

    Very small changes

--- PSD_ICC_Heritability.py
+++ PSD_ICC_Heritability.py
@@ -36,7 +36,7 @@ record_1 = pd.read_csv(os.path.join(DATA_PATH, "record_1_DK.csv"), index_col="Su
 record_2 = pd.read_csv(os.path.join(DATA_PATH, "record_2_DK.csv"), index_col="Subject_ID")
 record_3 = pd.read_csv(os.path.join(DATA_PATH, "record_3_DK.csv"), index_col="Subject_ID")
 
-# # Log the data
+# Log the data
 
 record_1 = np.log(record_1)
 record_2 = np.log(record_2)
